4db298442c390193283a940e459074f23dc601e3
Oleg Mihailik <mihailik@gmail.com>
2020-08-03 22:39:24 +0000

    With fancy routing.

--- demo-draw-share/index.js
+++ demo-draw-share/index.js
@@ -1,16 +1,15 @@
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
 
     if (req.query.name || (req.body && req.body.name)) {
         context.res = {
             // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
+            body: "Hello " + (req.query.name || req.body.name) + ' // ' + context.bindingData.text
         };
     }
     else {
         context.res = {
             status: 200,
-            body: "OK. But please pass a name on the query string or in the request body"
+            body: "OK. But please pass a name on the query string or in the request body // " + context.bindingData.text
         };
     }
 };
