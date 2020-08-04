8fb770ef584b25c5a1c56ea22c8a1ab02e31f10c
Oleg Mihailik <mihailik@gmail.com>
2020-08-04 17:41:44 +0000

    Color twitter implemented!

--- demo-draw-share/index.js
+++ demo-draw-share/index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-//const puppeteer = require('puppeteer');
+const { PNG } = require('pngjs');
 
 module.exports =
   /**
@@ -9,68 +9,95 @@ module.exports =
    */
   async function (context, req) {
     try {
-      //const script_text = '"ok"';
 
       const script_text = await new Promise((resolve, reject) => fs.readFile(
         path.resolve(__dirname, './app/index.js'),
         { encoding: 'utf-8' },
         (err, data) => err ? reject(err) : resolve(data)));
     
-      // let url = 'http://wikipedia.org/';
-      // if (typeof context.bindingData.text === 'string' && context.bindingData.text) {
-      //   url = /^(http:)|(https:)/i.test(context.bindingData.text) ?
-      //     context.bindingData.text :
-      //     'http://' + context.bindingData.text;
-      // }
-    
-      // const browser = await puppeteer.launch();
-      // const page = await browser.newPage();
-      // await page.goto(url);
-      // const screenshot = await page.screenshot();
+      let pngExtension = false;
+      const colorText = context.bindingData.text.replace(/\.png$/i, () => {
+        pngExtension = true;
+        return '';
+      });
 
-      // await browser.close();
+      const color32 = eval(colorText);
 
-      const pathText = context.bindingData.text;
+      const png = new PNG({ width: 800, height: 600 });
+      const r = (0xff0000 & color32) / 0x10000;
+      const g = (0x00ff00 & color32) / 0x100;
+      const b = (0x0000ff & color32);
 
-      let execResult = void 0;
-      try {
-        execResult = await eval(pathText || '');
-        if (typeof execResult === 'function')
-          execResult = await execResult();
+      const borderColor = r + g + b < 128 * 3 ? 255 : 0;
+      const borderWidth = 10;
+
+      for (let y = 0; y < png.height; y++) {
+        for (let x = 0; x < png.width; x++) {
+          const idx = (y * png.width + x) * 4;
+          if (Math.min(y, png.height - y, x, png.width - x) < borderWidth) {
+            png.data[idx] = borderColor;
+            png.data[idx + 1] = borderColor;
+            png.data[idx + 2] = borderColor;
+            png.data[idx + 3] = 255;
+          }
+          else {
+            png.data[idx] = r;
+            png.data[idx + 1] = g;
+            png.data[idx + 2] = b;
+            png.data[idx + 3] = 255;
+          }
+        }
       }
-      catch (error) {
-        execResult = {
-          message: error && error.message,
-          stack: error && error.stack,
-          constructor: error && error.constructor && error.constructor.name,
-          ...error
-        };
-      }
 
-      context.res = {
-        // status: 200, /* Defaults to 200 */
-        headers: {
-          'content-type': 'text/html'
-        },
-        body:
-          `
+      const buf = PNG.sync.write(png);
+      if (pngExtension) {
+        context.res = {
+          headers: {
+            'content-type': 'image/png'
+          },
+          body: buf
+        }
+      }
+      else {
+
+        const cardUrl = context.req.url.replace(/\?.*$/, '') + '.png';
+
+        const pngUri = 'data:image/png;base64,' + buf.toString('base64');
+        context.res = {
+          // status: 200, /* Defaults to 200 */
+          headers: {
+            'content-type': 'text/html'
+          },
+          body:
+            `
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Demo Draw Share ${pathText}</title>
+    <title>Demo Draw Share ${colorText}</title>
+
+<meta name="twitter:card" content="summary_large_image">
+<meta name="twitter:site" content="@demo-draw-share">
+<meta name="twitter:creator" content="@486timetable">
+<meta name="twitter:title" content="color:${colorText}">
+<meta name="twitter:description" content="PNG color of ${colorText}">
+<meta name="twitter:image" content="${cardUrl}">
+
 </head>
 <body>
 <script>
 ${script_text}
 </script>
-<h2>${pathText}</h2>
-<pre>${JSON.stringify(execResult, null, 2)}</pre>
+<h2>${colorText} r${r} g${g} b${b} borderColor${borderColor}</h2>
+<img src="${pngUri}">
+<hr>
+<img src="${cardUrl}">
 </body>
 </html>
 `
-      };
+        };
+      }
     }
     catch (error) {
       context.res = {
@@ -84,4 +111,5 @@ ${script_text}
         }, null, 2)
       };
     }
+    
   };
