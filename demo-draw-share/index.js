0febc97a7589ab107f106002de54b890268caab8
Oleg Mihailik <mihailik@gmail.com>
2020-08-13 22:09:06 +0000

    Minimal tidying of the code.

--- demo-draw-share/index.js
+++ demo-draw-share/index.js
@@ -10,11 +10,6 @@ module.exports =
   async function (context, req) {
     try {
 
-      const script_text = await new Promise((resolve, reject) => fs.readFile(
-        path.resolve(__dirname, './app/index.js'),
-        { encoding: 'utf-8' },
-        (err, data) => err ? reject(err) : resolve(data)));
-    
       let pngExtension = false;
       const colorText = context.bindingData.text.replace(/\.png$/i, () => {
         pngExtension = true;
@@ -23,81 +18,18 @@ module.exports =
 
 
       if (pngExtension) {
-
-        const color32 = parseColor(colorText);
-
-        const png = new PNG({ width: 800, height: 600 });
-        const r = (0xff0000 & color32) / 0x10000;
-        const g = (0x00ff00 & color32) / 0x100;
-        const b = (0x0000ff & color32);
-
-        const borderColor = r + g + b < 128 * 3 ? 255 : 0;
-        const borderWidth = 10;
-
-        for (let y = 0; y < png.height; y++) {
-          for (let x = 0; x < png.width; x++) {
-            const idx = (y * png.width + x) * 4;
-            if (Math.min(y, png.height - y, x, png.width - x) < borderWidth) {
-              png.data[idx] = borderColor;
-              png.data[idx + 1] = borderColor;
-              png.data[idx + 2] = borderColor;
-              png.data[idx + 3] = 255;
-            }
-            else {
-              png.data[idx] = r;
-              png.data[idx + 1] = g;
-              png.data[idx + 2] = b;
-              png.data[idx + 3] = 255;
-            }
-          }
-        }
-
-        const buf = PNG.sync.write(png);
-
         context.res = {
-          headers: {
-            'content-type': 'image/png'
-          },
-          body: buf
-        }
-      }
-      else {
-
-        const imageUrl = context.req.url.replace(/\?.*$/, '') + '.png';
-
-        context.res = {
-          headers: {
-            'content-type': 'text/html'
-          },
-          body:
-            `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Demo Draw Share ${colorText}</title>
-
-<meta name="twitter:card" content="summary_large_image">
-<meta name="twitter:site" content="@demo-draw-share">
-<meta name="twitter:creator" content="@486timetable">
-<meta name="twitter:title" content="color:${colorText}">
-<meta name="twitter:description" content="PNG color of ${colorText}">
-<meta name="twitter:image" content="${imageUrl}">
-<link rel="icon" type="image/png" href="${imageUrl}">
-
-</head>
-<body>
-<script>
-${script_text}
-</script>
-<h2>${colorText}</h2>
-<img src="${imageUrl}">
-</body>
-</html>
-`
+          headers: { 'content-type': 'image/png' },
+          body: getPngBuffer(colorText);
         };
       }
+      else {
+        context.res = {
+          headers: { 'content-type': 'text/html' },
+          body: getWebPageHTML(context.req.url, colorText)
+        };
+      }
+
     }
     catch (error) {
       context.res = {
@@ -113,6 +45,90 @@ ${script_text}
     }
     
   };
+
+/**
+ * @param {string} colorText
+ */
+function getPngBuffer(colorText) {
+
+  const color32 = parseColor(colorText);
+
+  const png = new PNG({ width: 800, height: 600 });
+  const r = (0xff0000 & color32) / 0x10000;
+  const g = (0x00ff00 & color32) / 0x100;
+  const b = (0x0000ff & color32);
+
+  const borderColor = r + g + b < 128 * 3 ? 255 : 0;
+  const borderWidth = 10;
+
+  for (let y = 0; y < png.height; y++) {
+    for (let x = 0; x < png.width; x++) {
+      const idx = (y * png.width + x) * 4;
+      if (Math.min(y, png.height - y, x, png.width - x) < borderWidth) {
+        png.data[idx] = borderColor;
+        png.data[idx + 1] = borderColor;
+        png.data[idx + 2] = borderColor;
+        png.data[idx + 3] = 255;
+      }
+      else {
+        png.data[idx] = r;
+        png.data[idx + 1] = g;
+        png.data[idx + 2] = b;
+        png.data[idx + 3] = 255;
+      }
+    }
+  }
+
+  const buf = PNG.sync.write(png);
+
+  return buf;
+}
+
+/**
+ * @param {string} fullUrl
+ * @param {string} colorText
+ */
+function getWebPageHTML(fullUrl, colorText) {
+
+  // May inject script from a file too:
+  // const script_text = await new Promise((resolve, reject) => fs.readFile(
+  //   path.resolve(__dirname, './app/index.js'),
+  //   { encoding: 'utf-8' },
+  //   (err, data) => err ? reject(err) : resolve(data)));
+
+  const imageUrl = fullUrl.replace(/\?.*$/, '') + '.png';
+
+  return `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Demo Draw Share ${colorText}</title>
+
+    <!-- Twitter -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@demo-draw-share">
+    <meta name="twitter:creator" content="@486timetable">
+    <meta name="twitter:title" content="color:${colorText}">
+    <meta name="twitter:description" content="PNG color of ${colorText}">
+    <meta name="twitter:image" content="${imageUrl}">
+
+    <!-- Favicon -->
+    <link rel="icon" type="image/png" href="${imageUrl}">
+
+</head>
+<body>${''/*  This, plus that commented piece above would inject script:
+<script>
+${script_text}
+</script>
+*/}
+<h2>${colorText}</h2>
+<img src="${imageUrl}">
+</body>
+</html>
+`;
+}
 
 function parseColor(colorText) {
   if (/^0x/i.test(colorText))
